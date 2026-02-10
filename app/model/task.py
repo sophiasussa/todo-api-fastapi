1472f9200a470ccd81bd61c321304d0b9f8a12be
sophiasussa <sophiasussa@outlook.com>
2026-02-10 10:50:24 -0300

    feat(model): add model layer for task entity

---
+++ app/model/task.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from app.database import Base
+
+class TaskModel(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100), nullable=False)
+    done = Column(Boolean, default=False)
+
