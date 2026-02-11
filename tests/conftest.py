a7683b5e10a6e6e02c922b09e42f9f4cf79749ef
sophiasussa <sophiasussa@outlook.com>
2026-02-11 16:37:16 -0300

    feat(tests): add test directory for task entity

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+from app.database.database import Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+TestingSessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+@pytest.fixture()
+def db_session():
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        Base.metadata.drop_all(bind=engine)
